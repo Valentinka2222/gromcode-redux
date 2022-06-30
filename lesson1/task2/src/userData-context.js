95d94ec6f52447c13f7833cb36fc89c226e279b9
Valentinka2222 <xamalyan89@gmail.com>
2022-06-30 20:16:13 +0300

    1

--- lesson1/task2/src/userData-context.js
+++ lesson1/task2/src/userData-context.js
@@ -4,4 +4,4 @@ export const userData = {
   name: 'Nicola Tesla',
   avatar_url: 'https://avatars3.githubusercontent.com/u10001',
 };
-export const UserDataContext = React.createContext({});
+export const UserDataContext = React.createContext();
