1cf33b7cf64c94a3426dcae05479d05ff2d8aeab
Valentinka2222 <xamalyan89@gmail.com>
2022-07-05 14:36:35 +0300

    4

--- lesson3/hometask1/src/language.reducer.js
+++ lesson3/hometask1/src/language.reducer.js
@@ -1,6 +1,6 @@
 import { LANGUAGE } from './language.actions';
 
-export const languageReducer = (state = '', action) => {
+export const languageReducer = (state = 'en', action) => {
   switch (action.type) {
     case LANGUAGE:
       return action.lang;
