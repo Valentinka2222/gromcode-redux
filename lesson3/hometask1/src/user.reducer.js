37d0c86d994197d1df1d4470e3d002e5be2d6574
Valentinka2222 <xamalyan89@gmail.com>
2022-07-05 10:43:57 +0300

    1

--- lesson3/hometask1/src/user.reducer.js
+++ lesson3/hometask1/src/user.reducer.js
@@ -1,26 +1,18 @@
-
-
-
 import { REMOVE_USER, SET_USER } from './user.actions';
 
 export const userReducer = (state ={}, action) => {
   switch (action.type) {
-    case SET_USER: {
+    case SET_USER: 
       return {
-       
-       user: action.payload.userData,
+        user: action.payload.userData
       };
       
-    }
-    case REMOVE_USER: {
+ 
+    case REMOVE_USER:
       return {
-     
-        
-      
-          user:delete[state.user[0]]
-   
+        user: delete [state.user[0]]
       };
-    }
+
 
     default:
       return state;
