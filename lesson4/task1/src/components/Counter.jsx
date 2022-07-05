cb41a15d6b305ab49b4f7417aa81ff9a2c18f444
Valentinka2222 <xamalyan89@gmail.com>
2022-07-05 16:46:17 +0300

    1

--- lesson4/task1/src/components/Counter.jsx
+++ lesson4/task1/src/components/Counter.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement, reset } from '../counter.actions';
-const Counter = ({ counter, incr, decr, res }) => {
+import * as counterActions from '../counter.actions';
+const Counter = ({ counter, increment, decrement, reset }) => {
   return (
     <div className="counter">
-      <button className="counter__button" onClick={decr}>
+      <button className="counter__button" onClick={decrement}>
         -
       </button>
-      <span className="counter__value" onClick={res}>
+      <span className="counter__value" onClick={reset}>
         {counter}
       </span>
-      <button className="counter__button" onClick={incr}>
+      <button className="counter__button" onClick={increment}>
         +
       </button>
     </div>
   );
 };
 
-const mapDispatch = dispatch => {
-  return {
-    incr: () => dispatch(increment()),
-    decr: () => dispatch(decrement()),
-    res: () => dispatch(reset()),
-  };
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
 };
 const mapState = state => {
   return {
