ca33958c34d693197171d097f1a5c44606597647
Valentinka2222 <xamalyan89@gmail.com>
2022-07-05 14:28:54 +0300

    2

--- lesson3/hometask1/src/store.js
+++ lesson3/hometask1/src/store.js
@@ -3,17 +3,14 @@ import { languageReducer } from './language.reducer';
 import userReducer from './user.reducer';
 import { cartReducer } from './cart.reducer';
  
-
-
-
 const appReducer = combineReducers({
   language: languageReducer,
   user: userReducer,
   cart: cartReducer,
 });
 
-const store = createStore(
+export const store = createStore(
   appReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-export default store;
+
