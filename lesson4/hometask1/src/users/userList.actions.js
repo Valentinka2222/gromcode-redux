66666e81c2dcfc0de5fdfd01fb0cc25f0c4aa23a
Valentinka2222 <xamalyan89@gmail.com>
2022-07-10 06:12:57 +0300

    1

--- lesson4/hometask1/src/users/userList.actions.js
+++ lesson4/hometask1/src/users/userList.actions.js
@@ -1,5 +1,5 @@
-export const PREV_PAGE = 'PAGE/PREV_PAGE';
-export const NEXT_PAGE = 'PAGE/NEXT_PAGE';
+export const PREV_PAGE = 'PREV_PAGE';
+export const NEXT_PAGE = 'NEXT_PAGE';
 
 export const prevPage = () => {
   return {
