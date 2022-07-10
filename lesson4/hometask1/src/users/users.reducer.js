096308a65bd05238f6567af5c3d5eabb73cf3068
Valentinka2222 <xamalyan89@gmail.com>
2022-07-10 11:37:01 +0300

    9

--- lesson4/hometask1/src/users/users.reducer.js
+++ lesson4/hometask1/src/users/users.reducer.js
@@ -1,10 +1,11 @@
-import { ADD_USER } from './users.actions';
+import { DELETE_USER, ADD_USER } from './users.actions';
 
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case ADD_USER:
-      return { usersList: action.usersList};
-
+      return { usersList: action.usersList };
+   case DELETE_USER:
+      return null;
     default:
       return state;
   }
