cbd156eb30378affa763f94586cceb22c02dac5f
Valentinka2222 <xamalyan89@gmail.com>
2022-07-10 05:35:08 +0300

    1

--- lesson4/hometask1/src/users/UsersList.jsx
+++ lesson4/hometask1/src/users/UsersList.jsx
@@ -28,8 +28,8 @@ class UsersList extends React.Component {
       <div>
         <Pagination
           currentPage={currentPage}
-          goNext={ this.nextBtnHandler }
-          goPrev={  this.prevBtnHandler }
+          goNext={this.nextBtnHandler }
+          goPrev={this.prevBtnHandler }
           totalItems={usersToRender.length}
           itemsPerPage={this.itemsPerPage}
         />
